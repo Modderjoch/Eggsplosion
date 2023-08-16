77776610befd9c3b4358907d1d40266c79882710
edwin <Edwinslottje@gmail.com>
2023-08-16 14:27:17 +0200

    First working version of "cumalative scoring"
    NOTE! : this only works for competitive ffa currently AND ONLY FOR ROUNDS WON!

--- Assets/SteamLeaderBoard.cs
+++ Assets/SteamLeaderBoard.cs
@@ -12,36 +12,89 @@ public class SteamLeaderBoard : MonoBehaviour
     private static bool s_initialized = false;
     private static CallResult<LeaderboardFindResult_t> m_findResult = new CallResult<LeaderboardFindResult_t>();
     private static CallResult<LeaderboardScoreUploaded_t> m_uploadResult = new CallResult<LeaderboardScoreUploaded_t>();
+    private static CallResult<LeaderboardScoresDownloaded_t> m_downloadResult = new CallResult<LeaderboardScoresDownloaded_t>();
 
     public bool isRoundOver = true;
     public RoundOverManager RoundOverManager = null;
     public ResultsManager ResultsManager = null;
+    private static int downloadedScore;
+
+    private static bool finishedDownloading = false;
+    private bool isRunningDownload = false;
     private void Awake()
     {
         
     }
     private void Start()
     {
-        GetSteamPlayerScore();
+        Init();
+        //GetSteamPlayerScore(); this is now on continue button
     }
-    public void GetSteamPlayerScore()
+
+    private void Update()
     {
-        if (isRoundOver && RoundOverManager != null)
+        if (s_initialized & !finishedDownloading &!isRunningDownload) 
         {
-            Debug.Log(RoundOverManager.playerConfigs[0].playerInput.devices[0].deviceId);
-
-            UpdateScore(RoundOverManager.playerConfigs[0].playerScore);
-        }
-        else if (!isRoundOver && ResultsManager != null)
-        {
-            Debug.Log(ResultsManager.playerConfigs[0].playerInput.devices[0].deviceId);
-
-            UpdateScore(ResultsManager.playerConfigs[0].playerScore);
+            DownloadSteamLeaderboardEntries();
+            isRunningDownload = true;
         }
     }
+
+
+    public void UpdateSteamPlayerScore()
+    {     
+        if (finishedDownloading)
+        {
+            if (isRoundOver && RoundOverManager != null)
+            {
+                Debug.Log(RoundOverManager.playerConfigs[0].playerInput.devices[0].deviceId);
+                Debug.Log(downloadedScore + " downloaded score!");
+                UpdateScore(RoundOverManager.playerConfigs[0].playerScore + downloadedScore);
+            }
+            else if (!isRoundOver && ResultsManager != null)
+            {
+                Debug.Log(ResultsManager.playerConfigs[0].playerInput.devices[0].deviceId);
+                Debug.Log(downloadedScore + " downloaded score!");
+                UpdateScore(ResultsManager.playerConfigs[0].playerScore + downloadedScore);
+             
+            }
+        }
+    }
+    public void DownloadSteamLeaderboardEntries()
+    {
+
+        Debug.Log("downloading entry??");
+        SteamAPICall_t hSteamAPICall = SteamUserStats.DownloadLeaderboardEntries(s_currentLeaderboard, ELeaderboardDataRequest.k_ELeaderboardDataRequestGlobalAroundUser, -1, 1);
+        m_downloadResult.Set(hSteamAPICall, OnLeaderboardScoresDownloaded);
+    }
+
+    static private void OnLeaderboardScoresDownloaded(LeaderboardScoresDownloaded_t pCallback, bool failure)
+    {
+        if (pCallback.m_cEntryCount == 0)
+        {
+            finishedDownloading = true;
+            return;
+        }
+        for (int index = 0; index < pCallback.m_cEntryCount; index++)
+        {
+         
+            LeaderboardEntry_t leaderboardEntry;
+            int[] details = { 0, 0, 0 };
+            SteamUserStats.GetDownloadedLeaderboardEntry(pCallback.m_hSteamLeaderboardEntries, index, out leaderboardEntry, details, 3);
+            UnityEngine.Debug.Log("STEAM LB ENTRY :  " + leaderboardEntry.m_nScore);
+            if (leaderboardEntry.m_steamIDUser == SteamUser.GetSteamID())
+            {
+                Debug.Log(leaderboardEntry.m_nScore);
+                downloadedScore = leaderboardEntry.m_nScore;
+                finishedDownloading = true;
+
+            }
+        }
+    }
+
     public void UpdateScore(int score)
     {
-        ;
+        
         if (!s_initialized)
         {
             UnityEngine.Debug.Log("Can't upload to the leaderboard because isn't loaded yet");
@@ -58,13 +111,14 @@ public class SteamLeaderBoard : MonoBehaviour
     {
         SteamAPICall_t hSteamAPICall = SteamUserStats.FindOrCreateLeaderboard(s_leaderboardName, ELeaderboardSortMethod.k_ELeaderboardSortMethodDescending, ELeaderboardDisplayType.k_ELeaderboardDisplayTypeNumeric);
         m_findResult.Set(hSteamAPICall, OnLeaderboardFindResult);
-        InitTimer();
+        //InitTimer();
     }
 
     static private void OnLeaderboardFindResult(LeaderboardFindResult_t pCallback, bool failure)
     {
         UnityEngine.Debug.Log("STEAM LEADERBOARDS: Found - " + pCallback.m_bLeaderboardFound + " leaderboardID - " + pCallback.m_hSteamLeaderboard.m_SteamLeaderboard);
         s_currentLeaderboard = pCallback.m_hSteamLeaderboard;
+
         s_initialized = true;
     }
 
@@ -72,7 +126,6 @@ public class SteamLeaderBoard : MonoBehaviour
     {
         UnityEngine.Debug.Log("STEAM LEADERBOARDS: failure - " + failure + " Completed - " + pCallback.m_bSuccess + " NewScore: " + pCallback.m_nGlobalRankNew + " Score " + pCallback.m_nScore + " HasChanged - " + pCallback.m_bScoreChanged);
     }
-
 
 
     private static Timer timer1;
@@ -85,4 +138,6 @@ public class SteamLeaderBoard : MonoBehaviour
     {
         SteamAPI.RunCallbacks();
     }
+
+  
 }
