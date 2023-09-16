8abdb252cb9b73d615f0a988e881b8d3bf821cad
Oebe Rademaker <499574@student.saxion.nl>
2023-09-16 17:16:09 +0200

    Fixed the UI in the Main Menu from disabling

---
+++ Assets/UIVariables.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIVariables : MonoBehaviour
+{
+    [SerializeField] private GameObject leaderboard;
+    [SerializeField] private GameObject leaderButton;
+    [SerializeField] private GameObject leaderOpen;
+    [SerializeField] private GameObject leaderClose;
+    [SerializeField] private Button eggsplanationButton;
+    [SerializeField] private GameObject playButton;
+
+    [SerializeField] private GameObject inGameCanvas;
+    [SerializeField] private GameObject inGameContinue;
+
+    private MenuHandler menuHandler;
+
+    protected void Awake()
+    {
+        AssignVariables();
+    }
+
+    private void AssignVariables()
+    {
+        menuHandler = UIInputManager.Instance.GetComponent<MenuHandler>();
+
+        if(leaderboard != null)
+        {
+            menuHandler.leaderboard = leaderboard;
+            menuHandler.leaderButton = leaderButton;
+            menuHandler.leaderOpen = leaderOpen;
+            menuHandler.leaderClose = leaderClose;
+            menuHandler.eggsplanationButton = eggsplanationButton;
+            menuHandler.playButton = playButton;
+        }
+        else if(inGameCanvas != null)
+        {
+            menuHandler.inGameCanvas = inGameCanvas;
+            menuHandler.inGameContinue = inGameContinue;
+        }
+        
+    }
+
+    public void OptionsOut()
+    {
+        EventSystem eventSystem = EventSystem.current;
+
+        inGameCanvas.SetActive(false);
+        eventSystem.SetSelectedGameObject(null);
+        Time.timeScale = 1.0f;
+        Debug.Log("Game unpaused");
+
+        menuHandler.isPaused = false;
+    }
+}
